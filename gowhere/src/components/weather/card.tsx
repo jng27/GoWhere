93dd4bc884d368b49e0827197dd2b1a87e45f0fe
EClaunchpad <58287805+jng27@users.noreply.github.com>
2023-12-21 02:17:52 +0800

    [FE][Add some basic styling for displaying weather information]

--- gowhere/src/components/weather/card.tsx
+++ gowhere/src/components/weather/card.tsx
@@ -2,7 +2,11 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import { WeatherForecast } from '../../views/types';
-// import AirOutlinedIcon from '@mui/icons-material/AirOutlined';
+import AirIcon from '@mui/icons-material/Air';
+import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
+import InvertColorsIcon from '@mui/icons-material/InvertColors';
+import CloudOutlinedIcon from '@mui/icons-material/CloudOutlined';
+import './card.css';
 
 interface props {
     elevation: number;
@@ -20,27 +24,48 @@ export default function Card(props: props) {
                 '& > :not(style)': {
                     m: 1,
                     width: 250,
-                    height: 400,
+                    height: 250,
                 },
             }}
         >
             <Paper {...props}>
                 <div style={{ padding: 10 }}>
-                    <p>Forecast: {forecast.forecast}</p>
-                    <p>
-                        Humidity:{' '}
-                        {JSON.stringify(forecast.relative_humidity.low)} -{' '}
-                        {JSON.stringify(forecast.relative_humidity.high)}
+                    <p
+                        id='card-p'
+                        style={{
+                            fontWeight: 700,
+                            fontSize: 16,
+                            textDecoration: 'underline',
+                        }}
+                    >
+                        Weather Forecast
                     </p>
-                    <p>
-                        Temperature: {JSON.stringify(forecast.temperature.low)}{' '}
-                        - {JSON.stringify(forecast.temperature.high)}
+                    <p id='card-p'>
+                        <CloudOutlinedIcon />
+                        <span id='card-span'> {forecast.forecast}</span>
                     </p>
-                    <p>Wind direction: {forecast.wind.direction}</p>
-                    <p>
-                        {/* <AirOutlinedIcon /> */}
-                        {JSON.stringify(forecast.wind.speed.low)} -{' '}
-                        {JSON.stringify(forecast.wind.speed.high)}
+                    <p id='card-p'>
+                        <InvertColorsIcon />
+                        <span id='card-span'>
+                            {JSON.stringify(forecast.relative_humidity.low)} -{' '}
+                            {JSON.stringify(forecast.relative_humidity.high)}
+                        </span>
+                    </p>
+                    <p id='card-p'>
+                        <DeviceThermostatIcon />
+                        <span id='card-span'>
+                            {' '}
+                            {JSON.stringify(forecast.temperature.low)} °C -{' '}
+                            {JSON.stringify(forecast.temperature.high)} °C
+                        </span>
+                    </p>
+                    <p id='card-p'>
+                        <AirIcon />
+                        <span id='card-span'>{forecast.wind.direction}, </span>
+                        <span id='card-span'>
+                            {JSON.stringify(forecast.wind.speed.low)} m/s -{' '}
+                            {JSON.stringify(forecast.wind.speed.high)} m/s
+                        </span>
                     </p>
                 </div>
             </Paper>
